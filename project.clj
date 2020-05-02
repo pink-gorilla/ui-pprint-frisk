88e43edfc057d92596603037e91f7da9bc30aa2a
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-05-02 12:52:54 -0500

    embedded-renderer

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
 
   :source-paths ["src"] ; "test"
   ;:test-paths ["test"]
-  
+
   :resource-paths  ["resources"
                     "node_modules/leaflet/dist"
                     "node_modules/mathjax/es5"
@@ -26,7 +26,7 @@
 
   :dependencies [;; OUR BUILD NEEDS TO BE UPDATED. WE HAVE DUPLICATE DEPENDENCIES
                  ;; DO NOT FORGET TO CHECK SHADOW-CLJS.EDN - IT CONTAINS SIMILAR DEPENDENCIES
-                 [reagent "0.10.0" 
+                 [reagent "0.10.0"
                   :exclusions [org.clojure/tools.reader
                                cljsjs/react
                                cljsjs/react-dom]]
@@ -34,7 +34,7 @@
                 ; [thheller/shadow-cljsjs "0.0.21"]
                  ;[com.taoensso/timbre "4.10.0"] ; clojurescript logging awb99: this fucks up kernel-cljs-shadowdeps
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"] ;; awb99: in encoding, and clj/cljs proof
-                 
+
                  [org.pinkgorilla/gorilla-renderable-ui "0.1.33"]]
 
   :profiles {:test {:source-paths ["src" "test"]
@@ -66,14 +66,12 @@
 
   :aliases {"clean"  ^{:doc "Cleans build artefacts."}
             ["shell" "./scripts/clean.sh"]
-
+            "bump-version" ^{:doc "Increases project.clj version number (used by CI)."}
+            ["change" "version" "leiningen.release/bump-version"]
             ;"build-shadow-ci" ["run" "-m" "shadow.cljs.devtools.cli" "compile" ":demo"] ; :ci
             ;"shadow-watch-demo" ["run" "-m" "shadow.cljs.devtools.cli" "watch" ":demo"]
             "build-test"  ^{:doc "Builds Bundle. Gets executed automatically before unit tests."}
             ["with-profile" "+test" "shell" "shadow-cljs" "compile" "ci"]
-
-            "demo"  ^{:doc "Runs UI components via webserver."}
-            ["shell" "shadow-cljs" "watch" "demo"]
 
             "test-run" ^{:doc "Runs unit tests. Does not build the bundle first.."}
             ["shell" "./node_modules/karma/bin/karma" "start" "--single-run"]
@@ -83,5 +81,8 @@
             "test-js" ^{:doc "Run Unit Tests. Will compile bundle first."}
             ["do" "build-test" ["test-run"]]
 
-            "bump-version" ^{:doc "Increases project.clj version number (used by CI)."}
-            ["change" "version" "leiningen.release/bump-version"]})
+            "demo"  ^{:doc "Runs UI components via webserver."}
+            ["shell" "shadow-cljs" "watch" "demo"]
+
+            "embed"  ^{:doc "Runs UI embedding via webserver."}
+            ["shell" "shadow-cljs" "watch" "embed"]})
