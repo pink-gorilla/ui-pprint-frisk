20fad332f61173af1b4e6f2ff6a0591a5c0d7308
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-05-26 22:55:26 -0500

    deps cleanup

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,13 +1,19 @@
-{:npm-deps {"leaflet" "^1.6.0"
-            "react-leaflet" "^2.6.0"
-            "ag-grid-community" "^22.0.0"
-            "ag-grid-react" "^22.0.0"
-            "react-dom-factories" "^1.0.2"
-            "react-sparklines" "^1.7.0"
-            "react-player" "^1.14.2"
-            "vega" "^5.11.1"
-            "vega-embed" "^6.7.0"
-            "vega-lite" "^4.11.0"
-            "highcharts" "^8.0.4"
-            "tailwindcss" "^1.4.6"
-            "react-grid-layout" "^0.18.3"}}
+{:npm-deps
+ {; shadow cljs creates package.json 
+  ; based on this dependencies 
+  "ag-grid-community" "^22.0.0"
+  "ag-grid-react" "^22.0.0"
+  "create-react-class" "^15.6.3"
+  "highcharts" "^8.0.4"
+  "leaflet" "^1.6.0"
+  "react" "^16.13.0"
+  "react-dom" "^16.13.0"
+  "react-dom-factories" "^1.0.2"
+  "react-grid-layout" "^0.18.3"
+  "react-leaflet" "^2.6.0"
+  "react-player" "^1.14.2"
+  "react-sparklines" "^1.7.0"
+  "tailwindcss" "^1.4.6"
+  "vega" "^5.11.1"
+  "vega-embed" "^6.7.0"
+  "vega-lite" "^4.11.0"}}
