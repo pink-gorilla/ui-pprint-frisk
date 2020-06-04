1da558d616a3c15c6c3ad0670fa808979c465db6
CI/CD <ci@pinkgorilla.org>
2020-06-04 02:41:43 +0000

    Begin 0.1.59-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-ui "0.1.58"
+(defproject org.pinkgorilla/gorilla-ui "0.1.59-SNAPSHOT"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/release_username
