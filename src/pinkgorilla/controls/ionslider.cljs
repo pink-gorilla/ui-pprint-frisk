ab11e57d1057e7188642d84b46c50195d328e1f9
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-06-03 21:57:14 -0500

    ionslider to bugfix

--- src/pinkgorilla/controls/ionslider.cljs
+++ src/pinkgorilla/controls/ionslider.cljs
@@ -1,13 +1,12 @@
 (ns pinkgorilla.controls.ionslider
   (:require
    [reagent.core :as r]
-   ["jquery" :as jq]
-   ["react-ion-slider" :as IonRangeSlider ;:refer [IonRangeSlider]
-    ]
+   ["jquery" :as jq] ; jquery is a dependency, but it needs to be required separately.
+   ["react-ion-slider" :as IonRangeSlider]
    [pinkgorilla.ui.pinkie :refer [register-tag]]))
 
 ; shiny: http://ionden.com/a/plugins/ion.rangeSlider/demo_interactions.html
-
+; https://www.npmjs.com/package/react-ion-slider#1-before-using-react-ion-slider-please-import-module
 
 ; type= {} min= {} max= {} from= {} to= {} step= {} values= {} keyboard= {}
 
@@ -32,9 +31,10 @@
    (let [on-change (fn [v]
                      (let [from (get v "from")
                            to (get v "to")]
-                       (println "slider value: from: " from "to: " to); (:from v))
+                       (println "slider value: from: " from "to: " to)
                        (swap! state assoc kw-from from)
-                       (when kw-to (swap! state assoc kw-from to))))
+                       (when kw-to 
+                         (swap! state assoc kw-to to))))
          options-to (if kw-to {:to (kw-to @state)
                                :type "double"} {})
          options (merge options
