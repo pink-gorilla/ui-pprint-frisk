22466038deb3d19b66fb6102f982be25c7ee1297
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-06-10 23:07:21 -0500

    jar resource check

--- scripts/show-resources.sh
+++ scripts/show-resources.sh
@@ -1,1 +1,5 @@
-jar tf target/gorilla-ui-0.1.23-SNAPSHOT.jar
+# jar tf target/gorilla-ui-0.1.23-SNAPSHOT.jar
+
+# ~/.m2/repository/org/pinkgorilla/gorilla-ui/0.2.9/gorilla-ui-0.2.9.jar
+
+jar tf ~/.m2/repository/org/pinkgorilla/gorilla-ui/0.2.10/gorilla-ui-0.2.10.jar
