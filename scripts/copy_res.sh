f5051206eec00762305e546b3d97110da86a7030
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-06-17 11:00:56 -0500

    copy_res mkdir

--- scripts/copy_res.sh
+++ scripts/copy_res.sh
@@ -1,4 +1,4 @@
 
 
-# mkdir target/node_modules/public/@fortawesome/fontawesome-free/webfonts
- cp node_modules/@fortawesome/fontawesome-free/webfonts/*  target/node_modules/public/@fortawesome/fontawesome-free/webfonts
+mkdir -p target/node_modules/public/@fortawesome/fontawesome-free/webfonts
+cp node_modules/@fortawesome/fontawesome-free/webfonts/*  target/node_modules/public/@fortawesome/fontawesome-free/webfonts
