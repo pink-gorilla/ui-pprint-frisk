3bb12b60fd520c0d5eaa968d8ba7ae916f0d743c
CI/CD <ci@pinkgorilla.org>
2020-07-15 09:27:03 +0000

    Release 0.2.27

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-ui "0.2.27-SNAPSHOT"
+(defproject org.pinkgorilla/gorilla-ui "0.2.27"
   :description "Reagent components for data visualisation."
   :url "https://github.com/pink-gorilla/gorilla-ui"
   :license {:name "MIT"}
