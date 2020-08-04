d459143240eb130664057becd1420336e423d901
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-08-04 21:02:03 +0200

    pinkie dep bump

--- project.clj
+++ project.clj
@@ -89,7 +89,7 @@
                   :exclusions [org.clojure/clojurescript]]
                  ;[com.taoensso/timbre "4.10.0"] ; clojurescript logging awb99: this fucks up kernel-cljs-shadowdeps
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"] ;; awb99: in encoding, and clj/cljs proof
-                 [org.pinkgorilla/pinkie "0.2.10"]]
+                 [org.pinkgorilla/pinkie "0.2.11"]]
 
   :profiles {:test {:source-paths ["src" "test"]
                     :test-paths   ["test"]}
