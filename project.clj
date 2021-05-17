f79dce3e836b898054c2b2f2d8ba2c779da2ef2d
CI/CD <ci@pinkgorilla.org>
2021-05-17 03:23:22 +0000

    Begin 0.3.21-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-ui "0.3.20"
+(defproject org.pinkgorilla/gorilla-ui "0.3.21-SNAPSHOT"
   :description "Reagent components for data visualisation."
   :url "https://github.com/pink-gorilla/gorilla-ui"
   :license {:name "MIT"}
