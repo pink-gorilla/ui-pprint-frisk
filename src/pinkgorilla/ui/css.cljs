012899a84a1914c9595ad7b0c5012d0bcbe27066
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-30 16:49:01 -0500

    partial edn theme

--- src/pinkgorilla/ui/css.cljs
+++ src/pinkgorilla/ui/css.cljs
@@ -15,21 +15,18 @@
    "Classic"
    "Material"])
 
+
+;; css config partially moved to edn config
+
+
 (def components
-  {:leaflet {true ["leaflet/dist/leaflet.css"]} ; "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-   :aggrid  (add-themes
+  {:aggrid  (add-themes
              {true ["ag-grid-community/dist/styles/ag-grid.css"]
               ;"default" ["ag-grid-community/dist/styles/ag-grid.css"]
               }
              "ag-grid-community/dist/styles/ag-theme-%s.css"
-             themes-aggrid)
-
-   :pydoc {true ["gorilla-ui/pydoc.css"]}})
+             themes-aggrid)})
 
 (def config
-  {:leaflet true
-   :aggrid   "balham" ; true 
-
-   :pydoc true})
-
-(println "gorilla-ui css config: " components)
+  {:aggrid   "balham" ; true 
+   })
